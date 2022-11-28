34af93dcc8daaa4de66475e86b13e6d031d36298
mahima <076bct033.mahima@pcampus.edu.np>
2022-11-28 16:17:17 +0545

    more changes

--- b.py
+++ b.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 a=np.array([0,1])
 b=np.array([1,0])
@@ -18,3 +19,12 @@ X=np.array([[1,0],[0,1]])
 Y=np.array([[2,1],[1,2]]) 
 Z=np.dot(X,Y)
 print(Z)
+
+
+x_train = np.array([1.0, 2.0,3,4])
+y_train = np.array([300.0, 500.0,600,900])
+plt.scatter(x_train, y_train, marker='x', c='r')
+plt.title("Housing Prices")
+plt.ylabel('Price (in 1000s of dollars)')
+plt.xlabel('Size (1000 sqft)')
+plt.show()
